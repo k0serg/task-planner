583feef83a72f442eb1fa9af229fc8c3b4f6aae5
k0serg <k0serg@internet.ru>
2025-06-30 16:01:58 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const ASSETS_TO_CACHE = [
   "/task-planner/",
   "/task-planner/index.html",
   "/task-planner/manifest.json",
-  "/task-planner/icon-192.png",
-  "/task-planner/icon-512.png"
+  "/task-planner/icons/icon-192.png",
+  "/task-planner/icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
